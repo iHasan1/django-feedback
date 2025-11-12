fe7d20b6371f3ea8aedfadd0fade63d84d490359
JBS <jbs@JBSs-MacBook-Pro.local>
2025-11-12 10:48:17 +0500

    added django's own form class for fomrs

--- reviews/views.py
+++ reviews/views.py
@@ -1,23 +1,22 @@
 from django.http import HttpResponseRedirect
 from django.shortcuts import render
 
+from .forms import ReviewForm
+
 # Create your views here.
 
 def review(request):
     if request.method == 'POST':
-        entered_username = request.POST['username']
+        form = ReviewForm(request.POST)
 
-        if entered_username == "":
-            return render(request, "reviews/review.html", {
-                "has_error": True
-            })
-        
-        print(entered_username)
-        # return render(request, "reviews/thank_you.html")
-        return HttpResponseRedirect("/thank-you")
+        if form.is_valid():
+            print(form.cleaned_data)
+            return HttpResponseRedirect("/thank-you")
+
+    form = ReviewForm()
 
     return render(request, "reviews/review.html", {
-        "has_error": False
+        "form": form
     })
 
 
