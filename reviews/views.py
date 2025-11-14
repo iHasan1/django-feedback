11a63b408c2a935c42be1941e8a41ddeed7ec159
JBS <jbs@JBSs-MacBook-Pro.local>
2025-11-14 05:35:17 +0500

    switched to ListView from TemplateView for ReviewList view

--- reviews/views.py
+++ reviews/views.py
@@ -2,6 +2,7 @@ from django.http import HttpResponseRedirect
 from django.shortcuts import render
 from django.views import View
 from django.views.generic.base import TemplateView
+from django.views.generic import ListView
 
 from .forms import ReviewForm
 from .models import Review
@@ -39,14 +40,10 @@ class ThankYouView(TemplateView):
     
 
 
-class ReviewsListView(TemplateView):
+class ReviewsListView(ListView):
     template_name = "reviews/review_list.html"
-
-    def get_context_data(self, **kwargs):
-        context = super().get_context_data(**kwargs)
-        reviews = Review.objects.all()
-        context["reviews"] = reviews
-        return context
+    model = Review
+    context_object_name = "reviews"
 
 
 class SingleReviewView(TemplateView):
