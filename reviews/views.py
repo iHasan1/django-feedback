cfeee39ca5e083b25ca536ce44260ebc24dc7e06
JBS <jbs@JBSs-MacBook-Pro.local>
2025-11-14 05:30:10 +0500

    added single review page using templateview

--- reviews/views.py
+++ reviews/views.py
@@ -28,6 +28,7 @@ class ReviewView(View):
             "form": form
         })
 
+
 class ThankYouView(TemplateView):
     template_name = "reviews/thank_you.html"
 
@@ -45,4 +46,15 @@ class ReviewsListView(TemplateView):
         context = super().get_context_data(**kwargs)
         reviews = Review.objects.all()
         context["reviews"] = reviews
+        return context
+
+
+class SingleReviewView(TemplateView):
+    template_name = "reviews/single_review.html"
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        review_id = kwargs["id"]
+        selected_review = Review.objects.get(pk=review_id)
+        context["review"] = selected_review
         return context
